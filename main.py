d88aadd0807af1853d791a36b39cf540500fbbeb
Ashutosh-AI <53949585+Ashutosh-AI@users.noreply.github.com>
2022-07-04 15:24:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from Detector import *
 detector = Detector(model_type="PS")   # pass here which type of task you want to do like (for object Detection OD), (for instance Segmentation IS), KP, PS
 
 
-#detector.onImage("data/image1.jpg")
+detector.onImage("data/image1.jpg")
 detector.onVideo("data/video.mp4")
